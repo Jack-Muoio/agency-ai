f21a5e73bf56b596d87def09ad90e37b84a15692
Jack-Muoio <jackmuoio81@gmail.com>
2026-03-31 10:28:47 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import TrustedBy from "./components/TrustedBy.jsx";
+import Services from "./components/Services.jsx";
+
+const App = () => {
+  const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
+
+  return (
+    <div className='dark:bg-black relative'>
+        <Navbar theme={theme} setTheme={setTheme} />
+        <Hero />
+        <TrustedBy />
+        <Services />
+    </div>
+  );
+};
+
+export default App;
